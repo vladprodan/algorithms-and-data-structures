d4c1345fd1f23f795b31a9c1e12c8fd59ae3a812
Vladyslav Prodan <imvladprodan@gmail.com>
2023-10-15 20:58:34 +0300

    Add example of binary search

---
+++ simpleBinarySearch.js
@@ -0,0 +1,36 @@
+const binarySearch = (items, item) => {
+  let left = 0;
+  let right = items.length - 1;
+
+  while (left <= right) {
+    const centerIndex = Math.floor((left + right) / 2);
+
+    if (items[centerIndex] === item) {
+      console.log(centerIndex);
+      return centerIndex;
+    }
+
+    if (items[centerIndex].localeCompare(item) === 1) {
+      right = centerIndex - 1;
+    } else {
+      left = centerIndex + 1;
+    }
+  }
+
+  return null;
+};
+
+const plants = [
+  "Аспарагус",
+  "Гвоздика",
+  "Жасмин",
+  "Калина",
+  "Малина",
+  "Пион",
+  "Тысячелистник",
+  "Хризантема",
+  "Шафран",
+  "Юкка",
+]
+
+binarySearch(plants, "Пион"); // 5
